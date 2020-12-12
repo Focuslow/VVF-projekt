8b64ec872c1cc79b5ee602638ab7770a4e1dc5e3
Focuslow <kubik545@gmail.com>
2020-12-12 18:15:29 +0100

    big boi

--- main.py
+++ main.py
@@ -1,1 +1,129 @@
-#ejo
+from PyQt5 import QtGui, QtWidgets, QtCore
+from mnc import mnc
+import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 20, 850, 563))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(4, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setEnabled(True)
+        self.label.setMinimumSize(QtCore.QSize(300, 100))
+        self.label.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout_3.addWidget(self.label)
+
+        self.lineEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout_3.addWidget(self.lineEdit)
+
+        self.line_2 = QtWidgets.QFrame(self.horizontalLayoutWidget)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.line_2.setLineWidth(10)
+        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_2.setObjectName("line_2")
+        self.verticalLayout_3.addWidget(self.line_2)
+
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.verticalLayout_3.addWidget(self.lineEdit_2)
+        self.verticalLayout.addLayout(self.verticalLayout_3)
+
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+        self.label_2 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+
+        self.radioButton_3 = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton_3.setObjectName("radioButton_3")
+        self.horizontalLayout_2.addWidget(self.radioButton_3)
+
+        self.radioButton_2 = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.horizontalLayout_2.addWidget(self.radioButton_2)
+
+        self.radioButton = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton.setObjectName("radioButton")
+        self.horizontalLayout_2.addWidget(self.radioButton)
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.verticalLayout.addLayout(self.verticalLayout_2)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.btn = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.btn.setText("Run")
+        self.verticalLayout.addWidget(self.btn)
+
+        self.namiestoTohtoTrebaGraf = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.namiestoTohtoTrebaGraf.setAlignment(QtCore.Qt.AlignCenter)
+        self.namiestoTohtoTrebaGraf.setWordWrap(False)
+        self.namiestoTohtoTrebaGraf.setObjectName("namiestoTohtoTrebaGraf")
+        self.horizontalLayout.addWidget(self.namiestoTohtoTrebaGraf)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.conn_btns()
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Zadaj systém"))
+        self.label_2.setText(_translate("MainWindow", "Vyber vstupný signál"))
+        self.radioButton_3.setText(_translate("MainWindow", "Niečo"))
+        self.radioButton_2.setText(_translate("MainWindow", "Step"))
+        self.radioButton.setText(_translate("MainWindow", "Sin"))
+        self.namiestoTohtoTrebaGraf.setText(_translate("MainWindow", "fdsfdsfsdssssssssssssssssssssssssssssssssssssssssssssssssssss"))
+
+    def keyPressEvent(self, event):
+        if type(event) == QtGui.QKeyEvent:
+            if event.key() == QtCore.Qt.Key_Escape:
+                sys.exit(0)
+
+    def conn_btns(self):
+        self.btn.clicked.connect(self.start)
+
+    def start(self):
+        num = self.lineEdit.text()
+        den = self.lineEdit_2.text()
+
+        sys=[list(num.split(",")),list(den.split(","))]
+
+        mnc(sys)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
--- mnc.py
+++ mnc.py
@@ -6,7 +6,8 @@ import time
 from matplotlib import pyplot as plt
 
 
-def mnc2(na, nb, num, den, ts, t_max, ax):
+def mnc_calc(na, nb, num, den, ts, t_max, ax, u=None):
+
     if na < 1 or nb < 1 or np.floor(na) != na or np.floor(nb) != nb:
         sumee = 1e9
         return sumee, ax
@@ -15,10 +16,11 @@ def mnc2(na, nb, num, den, ts, t_max, ax):
     Gs = Transfer(num, den)
 
     try:
-        u = np.concatenate((np.array([0]), np.ones(len(t)-1)))
-        pom = round(len(u) / 10)
-        u[3 * pom:5 * pom] = 0
-        u = u + 0.03 * np.random.rand(np.size(u)) + np.sin(2 * np.pi / 10 * t)
+        if not u:
+            u = np.concatenate((np.array([0]), np.ones(len(t)-1)))
+            pom = round(len(u) / 10)
+            u[3 * pom:5 * pom] = 0
+            u = u + 0.03 * np.random.rand(np.size(u)) + np.sin(2 * np.pi / 10 * t)
 
         y = simulate_linear_system(Gs, u, t)[0]
         y.resize((len(y)), refcheck=False)
@@ -60,7 +62,7 @@ def mnc2(na, nb, num, den, ts, t_max, ax):
         # ax[1].get_lines()[1].set_xdata(t)
         # ax[1].get_lines()[1].set_ydata(simulate_linear_system(Gs_ident, u, t)[0])
         # plt.show()
-    
+
         #ax2.plot(t, y, 'b', t, simulate_linear_system(Gs_ident, u, t)[0], 'r')
 
         eee = FI.dot(parametry) - Y
@@ -73,32 +75,40 @@ def mnc2(na, nb, num, den, ts, t_max, ax):
         sumee = 1e9
         return sumee, ax
 
+def mnc(sys=None,t=None, u=None):
 
-# fig, (ax1, ax2) = plt.subplots(2, 1)
-# ax = [ax1, ax2]
-# line1, = ax1.plot([0], [0], 'k')
-# line2, = ax1.plot([0], [0], 'b')
-# line3, = ax2.plot([0], [0], 'b')
-# line4, = ax2.plot([0], [0], 'r')
+    # line1, = ax1.plot([0], [0], 'k')
+    # line2, = ax1.plot([0], [0], 'b')
+    # line3, = ax2.plot([0], [0], 'b')
+    # line4, = ax2.plot([0], [0], 'r')
 
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax = [ax1, ax2]
 
-results = 1e9 * np.ones((10, 10))
+    results = 1e9 * np.ones((10, 10))
+    if not sys:
+        num = np.array([1, 2])
+        den = np.array([1, 4, 3, 5, 1])
+    else:
+        num = np.array(sys[0])
+        den = np.array(sys[1])
 
-num = np.array([1, 2])
-den = np.array([1, 4, 3, 5, 1])
-ts = 0.1
-t_max = 50
+    if not t:
+        ts = 0.1
+        t_max = 50
+    else:
+        ts=t[0]
+        t_max=t[1]
 
-for i in range(1, 11):
-    for j in range(1, i + 1):
-        results[i - 1, j - 1], ax = mnc2(i, j, num, den, ts, t_max, ax)
+    for i in range(1, 11):
+        for j in range(1, i + 1):
+            results[i - 1, j - 1], ax = mnc_calc(i, j, num, den, ts, t_max, ax,u)
 
-min_val = np.amin(results)
-coords_i = np.where(results == min_val)[0]
-coords_j = np.where(results[coords_i,:][0] == min_val)[0]
-print('value: ' + str(min_val))
-print('a: ' + str(coords_i[0]) + '   b: ' + str(coords_j[0]))
+    min_val = np.amin(results)
+    coords_i = np.where(results == min_val)[0]
+    coords_j = np.where(results[coords_i,:][0] == min_val)[0]
+    print('value: ' + str(min_val))
+    print('a: ' + str(coords_i[0]) + '   b: ' + str(coords_j[0]))
 
-time.sleep(3)
-mnc2(coords_i[0], coords_j[0], num, den, ts, t_max, ax)
-
+    time.sleep(3)
+    mnc_calc(coords_i[0], coords_j[0], num, den, ts, t_max, ax)
